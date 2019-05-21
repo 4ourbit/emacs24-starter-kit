3fc67fb6c029c31c2be083126c38278566902df6
4ourbit <4ourbit@gmail.com>
2019-05-21 17:05:07 +0200

    Emacs25 compatibility renaming and reordering.

--- init.el
+++ init.el
@@ -6,6 +6,13 @@
 ;;
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (define-key input-decode-map (kbd "M-?") (kbd "C-h"))
 (define-key input-decode-map (kbd "C-k") (kbd "<up>"))
 (define-key input-decode-map (kbd "C-j") (kbd "<down>"))
