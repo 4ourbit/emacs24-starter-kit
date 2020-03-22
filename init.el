fce213f132e7a7861d1f0256e6ddd4f8b12f31af
4ourbit <4ourbit@gmail.com>
2020-03-22 12:25:42 +0000

    input-decode-map: ignore prefix movement keys
    this is to free up e.g. C-c C-k as quit-select-log in magit

--- init.el
+++ init.el
@@ -14,11 +14,34 @@
 (package-initialize)
 
 (define-key input-decode-map (kbd "M-?") (kbd "C-h"))
+
+(define-key input-decode-map (kbd "C-c C-k") (kbd "C-c C-k"))
+(define-key input-decode-map (kbd "C-x C-k") (kbd "C-x C-k"))
 (define-key input-decode-map (kbd "C-k") (kbd "<up>"))
+(define-key input-decode-map (kbd "C-c C-j") (kbd "C-c C-j"))
+(define-key input-decode-map (kbd "C-x C-j") (kbd "C-x C-j"))
 (define-key input-decode-map (kbd "C-j") (kbd "<down>"))
+(define-key input-decode-map (kbd "C-c C-l") (kbd "C-c C-l"))
+(define-key input-decode-map (kbd "C-x C-l") (kbd "C-x C-l"))
 (define-key input-decode-map (kbd "C-l") (kbd "C-<left>"))
+(define-key input-decode-map (kbd "C-c C-ö") (kbd "C-c C-ö"))
+(define-key input-decode-map (kbd "C-x C-ö") (kbd "C-x C-ö"))
 (define-key input-decode-map (kbd "C-ö") (kbd "C-<right>"))
+(define-key input-decode-map (kbd "C-c C-;") (kbd "C-c C-;"))
+(define-key input-decode-map (kbd "C-x C-;") (kbd "C-x C-;"))
 (define-key input-decode-map (kbd "C-;") (kbd "C-<right>"))
+
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Translation-Keymaps.html
+;; 22.14.1 Interaction with normal keymaps
+;; ..., it is better to avoid binding commands to key sequences where the end of
+;; the key sequence is a prefix of a key translation. The main such problematic
+;; suffixes/prefixes are <ESC>, M-O (which is really <ESC> O) and M-[ (which is
+;; really <ESC> [).
+
+(define-key input-decode-map (kbd "C-ü") (kbd "<ESC>"))
+;; This is where C-[ is located at in de-de keyboard layout replace it with the
+;; en-us standard behavior to block it from a future use in user-level key
+;; binding maps, in case of switching keyboard layouts.
 
 (define-key input-decode-map (kbd "C-M-j") (kbd "<next>"))
 (define-key input-decode-map (kbd "C-M-k") (kbd "<prior>"))
@@ -27,10 +50,6 @@
 (define-key input-decode-map (kbd "C-M-;") (kbd "<end>"))
 
 (define-key input-decode-map (kbd "C-h") (kbd "<backspace>"))
-;; (define-key input-decode-map (kbd "C-M-h") (kbd "C-<backspace>"))
-;; (define-key my-keys-minor-mode-map (kbd "C-M-h") 'backward-kill-word)
-;; (define-key my-keys-minor-mode-map (kbd "M-ö") 'backward-kill-word)
-;; (define-key input-decode-map (kbd "C-m") (kbd "<return>"))
 
 (add-to-list 'load-path (locate-user-emacs-file "src/"))
 
