1fca5b921d509b09fe58e1a04983947a7b9df390
4ourbit <4ourbit@gmail.com>
2020-03-26 09:34:42 +0000

    remove obsolete package-initialize call
    sk will call it later on

--- init.el
+++ init.el
@@ -6,12 +6,11 @@
 ;;
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html
 
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;(package-initialize)
 
 (define-key input-decode-map (kbd "M-?") (kbd "C-h"))
 
